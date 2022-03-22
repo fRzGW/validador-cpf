0b1df860397675d23e153127f1da55f289ad9aa1
jaovitokkk <92704272+fRzGW@users.noreply.github.com>
2022-03-21 22:28:11 -0300

    Adicionando arquivos iniciais

---
+++ app.js
@@ -0,0 +1,66 @@
+const submit = document.querySelector('.submit');
+const result = document.querySelector('.result');
+
+submit.addEventListener('click', () => {
+    let getCpf = document.querySelector('.cpf').value;
+    let getCPF = new ValidaCPF(String(getCpf));
+
+    console.log(getCpf);
+    if(getCPF.valida()){
+        result.innerHTML = 'CPF Válido';
+    } else {
+        result.innerHTML = 'CPF Inválido';
+    }
+});
+
+
+function ValidaCPF(cpfEnviado) {
+    Object.defineProperty(this, 'cpfLimpo', {
+        enumerable: true,
+        get: function(){
+            return cpfEnviado.replace(/\D+/g, ''); 
+        }
+    });
+}
+
+ValidaCPF.prototype.valida = function() {
+    if(typeof this.cpfLimpo === 'undefined') return false;
+    if(this.cpfLimpo.length !== 11) return false;
+    if(this.isSequencial()) return false;
+    
+    const cpfParcial = this.cpfLimpo.slice(0, -2);
+    const digitoA = this.criaDigito(cpfParcial);
+    const digitoB = this.criaDigito(cpfParcial + digitoA);
+
+    const novoCpf = cpfParcial + digitoA + digitoB;
+
+    return novoCpf === this.cpfLimpo;
+};
+
+ValidaCPF.prototype.criaDigito = function(cpfParcial) {
+    const cpfArray = Array.from(cpfParcial);
+    let regressivo = cpfArray.length + 1;
+
+    const total = cpfArray.reduce((ac, val) => {
+        ac += (regressivo * Number(val));
+        regressivo--;
+        return ac;
+    }, 0);
+
+    const digito = 11 - (total % 11);
+    return digito > 9 ? '0' : String(digito);
+};
+
+ValidaCPF.prototype.isSequencial = function() {
+    const sequencia = this.cpfLimpo[0].repeat(this.cpfLimpo.length);
+    return sequencia === this.cpfLimpo;
+}
+
+// const cpf = new ValidaCPF('705.484.450-52');
+
+// if(cpf.valida()){
+//     console.log('CPF Válido');
+// } else {
+//     console.log('CPF Inválido');
+// }
+
